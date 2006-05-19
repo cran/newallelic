213b864194784f6878a371f415e7d8781b8c51c5
Mickael Guedj <guedj@genopole.cnrs.fr>
2006-05-19 00:00:00 +0000

    version 0.1

---
+++ R/newallelic.R
@@ -0,0 +1,5 @@
+
+newallelic <- function(d0,d1,d2,h0,h1,h2) {
+  return( .C("newallelic",as.integer(d0),as.integer(d1),as.integer(d2),as.integer(h0),as.integer(h1),
+          as.integer(h2),pv=as.double(0))$pv);
+}
